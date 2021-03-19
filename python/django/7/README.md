06a13a5bc8efcc09b69cbc5d46a35d0e0f78b5ce
avriis <riis@prescriba.com>
2021-03-19 15:43:18 +0100

    Small change

--- python/django/7/README.md
+++ python/django/7/README.md
@@ -1,1 +1,4 @@
 # Test django models
+
+https://docs.djangoproject.com/en/1.11/topics/testing/overview/
+https://docs.djangoproject.com/en/1.11/intro/tutorial02/
--- python/django/7/project/app/tests.py
+++ python/django/7/project/app/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Question
+import datetime
 
 class FirstTest(TestCase):
     def first_test(self):
@@ -12,4 +14,12 @@ class ViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Hello, world")
 
+
 class ModelTests(TestCase):
+    def setUp(self):
+        self.question_text = "Delicious question text"
+        Question.objects.create(question_text=self.question_text, pub_date=datetime.datetime.now())
+
+    def test_question_text(self):
+        question = Question.objects.get(question_text=self.question_text)
+        self.assertEqual(question.question_text, self.question_text)
